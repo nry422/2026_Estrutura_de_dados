17f7c2e20ba4320808a3d2fac9dae0d338c01aba
nry422 <henrymuller@gmail.com>
2026-05-28 18:41:55 -0300

    do_while_struct_matricula

--- do_while_menu_matricula.c
+++ do_while_menu_matricula.c
@@ -1,72 +1,118 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define MAX 100
 
 int opcao()
 {
-   int op;
-   printf("Matriculas de alunos\nInsira a opcao:\n [0] para sair\n [1] para incluir\n [2] para listar\n Opcao : ");
-   scanf("%d", &op);
-   system("cls");
-   return op;
+    system("pause");
+    system("cls");
+    int op;
+    printf("Matriculas de alunos\nInsira a opcao:\n [0] para sair\n [1] para incluir\n [2] para listar\n [3] para buscar : ");
+    scanf("%d", &op);
+    system("cls");
+    return op;
 }
 
-typedef struct {
-   int matr;
-   char nome[50];
-   char sexo;
-   float alt;
+typedef struct
+{
+    int matr;
+    char nome[50];
+    char sexo;
+    float alt;
 } tAluno;
+
+void meuStrToUpper(char *str)
+{
+    for (int i=0; i<strlen(str); i++)
+    {
+        if(str[i]>='a' && str[i]<='z')
+        {
+            str[i] -= 32;
+        }
+    }
+}
 
 int main()
 {
-   int op, i, qtd=0, iMatr;
-   tAluno vAluno[MAX];
+    int op, i, qtd=0, iMatr, encontrados=0;
+    tAluno vAluno[MAX];
+    char busca[MAX];
 
-   do {
-      op = opcao();
+    do
+    {
+        op = opcao();
 
-      switch (op) {
-      case 1:
-         printf("Inclusao\n");
-         printf("Guardar dados dos alunos em struct com vetores no while\n Matricula = 0 para parar!\n\n\n");
-         printf("Matricula: ");
-         scanf("%d", &iMatr);
-
-         while(iMatr != 0) {
-            vAluno[i].matr= iMatr;
-
-            printf("Nome: ");
-            scanf(" %[^\n]", &vAluno[qtd].nome);
-            printf("Sexo: ");
-            scanf(" %c", &vAluno[qtd].sexo);
-            printf("Altura: ");
-            scanf("%f", &vAluno[qtd].alt);
-
-            qtd++;
-            //proximo loop
-            printf("Matricula:");
+        switch (op)
+        {
+        case 1:
+            printf("Inclusao\n");
+            printf("Guardar dados dos alunos em struct com vetores no while\n Matricula = 0 para parar!\n\n\n");
+            printf("Matricula: ");
             scanf("%d", &iMatr);
+
+            while(iMatr != 0)
+            {
+                vAluno[qtd].matr = iMatr;
+
+                printf("Nome: ");
+                scanf(" %[^\n]", vAluno[qtd].nome);
+                meuStrToUpper(vAluno[qtd].nome);
+                printf("Sexo: ");
+                scanf(" %c", &vAluno[qtd].sexo);
+                printf("Altura: ");
+                scanf("%f", &vAluno[qtd].alt);
+
+                qtd++;
+                //proximo loop
+                printf("Matricula:");
+                scanf("%d", &iMatr);
+            }
+
+            break;
+
+
+        case 2:
+            printf("Listar\n");
+            for(i=0; i<qtd; i++)
+            {
+                printf("%d, %s, %c, %.2f.\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
+            }
+
+            break;
+
+         case 3:
+         encontrados = 0;
+         printf("Buscar\n");
+         printf("Digite aqui sua busca\n");
+         scanf(" %[^\n]", busca);
+         meuStrToUpper(busca);
+
+         for(i = 0; i < qtd; i++) {
+            if(strstr(vAluno[i].nome, busca) != NULL) {
+               printf("Matricula : %d\n Nome : %s\n Sexo: %c\n Altura : %.2f\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
+               encontrados++;
+            }
          }
 
-         break;
+         if(encontrados == 0) {
+            printf("Não foi encontrado!!\n");
 
-
-      case 2:
-         printf("Listar\n");
-         for(i=0; i<qtd; i++) {
-            printf("%d, %s, %c, %.2f.\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
          }
+         else {
+            printf("Total: %d Alunos Encontrados\n", encontrados);
 
+         }
          break;
 
-      case 0:
-         printf("Saindo...\n");
-         break;
+        case 0:
+            printf("Saindo...\n");
+            break;
 
-      default:
-         printf("opcao invalida");
-      }
-   }
-   while (op != 0);
+        default:
+            printf("opcao invalida");
+        }
+    }
+    while (op != 0);
 }
 
