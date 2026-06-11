2671afbd37942f87872efc4da922c5d83e4629a7
nry422 <henrymuller@gmail.com>
2026-06-11 16:10:50 -0300

    aula 11/06/2026

--- do_while_menu_matricula.c
+++ do_while_menu_matricula.c
@@ -5,83 +5,88 @@
 
 int opcao()
 {
-    system("pause");
-    system("cls");
-    int op;
-    printf("Matriculas de alunos\nInsira a opcao:\n [0] para sair\n [1] para incluir\n [2] para listar\n [3] para buscar : ");
-    scanf("%d", &op);
-    system("cls");
-    return op;
+   system("pause");
+   system("cls");
+   int op;
+   printf("Matriculas de alunos\nInsira a opcao:\n [0] Sair\n [1] Incluir\n [2] Listar\n [3] Buscar\n [4] Gerar Arquivo CSV\n [5] Carregar Arquivo\n Sua opcao : ");
+   scanf("%d", &op);
+   system("cls");
+   return op;
 }
 
-typedef struct
-{
-    int matr;
-    char nome[50];
-    char sexo;
-    float alt;
+typedef struct {
+   int matr;
+   char nome[50];
+   char sexo;
+   float alt;
 } tAluno;
+
+void firula(int i)
+{
+   if (i%4==0) printf("-");
+   if (i%4==1) printf("\\");
+   if (i%4==2) printf("|");
+   if (i%4==3) printf("/");
+   Sleep(150);
+   printf("\b");
+}
 
 void meuStrToUpper(char *str)
 {
-    for (int i=0; i<strlen(str); i++)
-    {
-        if(str[i]>='a' && str[i]<='z')
-        {
-            str[i] -= 32;
-        }
-    }
+   for (int i=0; i<strlen(str); i++) {
+      if(str[i]>='a' && str[i]<='z') {
+         str[i] -= 32;
+      }
+   }
 }
 
 int main()
 {
-    int op, i, qtd=0, iMatr, encontrados=0;
-    tAluno vAluno[MAX];
-    char busca[MAX];
+   int op, i, qtd=0, iMatr, encontrados=0;
+   tAluno vAluno[MAX];
+   char busca[MAX];
+   char filename[] = "aluno.csv";
+   FILE *file;
 
-    do
-    {
-        op = opcao();
+   do {
+      op = opcao();
 
-        switch (op)
-        {
-        case 1:
-            printf("Inclusao\n");
-            printf("Guardar dados dos alunos em struct com vetores no while\n Matricula = 0 para parar!\n\n\n");
-            printf("Matricula: ");
+      switch (op) {
+      case 1:
+         printf("Inclusao\n");
+         printf("Guardar dados dos alunos em struct com vetores no while\n Matricula = 0 para parar!\n\n\n");
+         printf("Matricula: ");
+         scanf("%d", &iMatr);
+
+         while(iMatr != 0) {
+            vAluno[qtd].matr = iMatr;
+
+            printf("Nome: ");
+            scanf(" %[^\n]", vAluno[qtd].nome);
+            meuStrToUpper(vAluno[qtd].nome);
+            printf("Sexo: ");
+            scanf(" %c", &vAluno[qtd].sexo);
+            printf("Altura: ");
+            scanf("%f", &vAluno[qtd].alt);
+
+            qtd++;
+            //proximo loop
+            printf("Matricula:");
             scanf("%d", &iMatr);
+         }
 
-            while(iMatr != 0)
-            {
-                vAluno[qtd].matr = iMatr;
-
-                printf("Nome: ");
-                scanf(" %[^\n]", vAluno[qtd].nome);
-                meuStrToUpper(vAluno[qtd].nome);
-                printf("Sexo: ");
-                scanf(" %c", &vAluno[qtd].sexo);
-                printf("Altura: ");
-                scanf("%f", &vAluno[qtd].alt);
-
-                qtd++;
-                //proximo loop
-                printf("Matricula:");
-                scanf("%d", &iMatr);
-            }
-
-            break;
+         break;
 
 
-        case 2:
-            printf("Listar\n");
-            for(i=0; i<qtd; i++)
-            {
-                printf("%d, %s, %c, %.2f.\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
-            }
+      case 2:
+         printf("Listar\n");
+         for(i=0; i<qtd; i++) {
+            printf("%d, %s, %c, %.2f.\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
+         }
 
-            break;
+         break;
 
-         case 3:
+      case 3:
          encontrados = 0;
          printf("Buscar\n");
          printf("Digite aqui sua busca\n");
@@ -105,14 +110,48 @@ int main()
          }
          break;
 
-        case 0:
-            printf("Saindo...\n");
+      case 4:
+         printf("Gerando Arquivo CSV...");
+         file = fopen(filename, "w");
+         if (file==NULL) {
+            printf("Erro ao abrir o arquivo $s!!!\n", filename);
             break;
+         }
+         for(i=0; i<qtd; i++) {
+            fprintf(file, "%d;%s;%c;%.2f\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
+            firula(i);
+         }
+         fclose(file);
+         printf("Arquivo salvo com sucesso!!!\n");
+         break;
 
-        default:
-            printf("opcao invalida");
-        }
-    }
-    while (op != 0);
+      case 5:
+         qtd = 0;
+         printf("Carregando Arquivo...\n");
+         file = fopen(filename, "r");
+
+         if (file==NULL) {
+            printf("Erro ao abrir o arquivo $s!!!\n", filename);
+            break;
+         }
+         while (fscanf(file, "%d;%[^;];%c;%f\n",
+                       &vAluno[qtd].matr, &vAluno[qtd].nome, &vAluno[qtd].sexo, &vAluno[qtd].alt) == 4) {
+            printf("%d, %s, %c, %.2f.\n", vAluno[qtd].matr, vAluno[qtd].nome, vAluno[qtd].sexo, vAluno[qtd].alt);
+            qtd++;
+         }
+
+         fclose(file);
+         printf("%d alunos carregados com sucesso!\n", qtd);
+         break;
+
+      case 0:
+         printf("Saindo...\n");
+         break;
+
+      default:
+         printf("opcao invalida");
+      }
+   }
+   while (op != 0);
 }
 
